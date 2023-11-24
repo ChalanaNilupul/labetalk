2b31f87bc5610a484cec7f7694478abdfb0efba8
ChalanaNilupul <senarathnacn@gmail.com>
2023-11-24 16:47:02 +0530

    home

---
+++ client/js/navBar.js
@@ -0,0 +1,7 @@
+function nav(){
+    var nav = document.getElementById('navBarMobile');
+    var list = document.getElementById('min');
+
+    nav.classList.toggle('active');
+    list.classList.toggle("anim");
+}
