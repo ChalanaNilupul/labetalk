9f0b93437896e12a916a4c1d24d95fcb8347695b
ChalanaNilupul <senarathnacn@gmail.com>
2024-01-18 09:02:42 +0530

    finalize

--- client/pages/addAd.php
+++ client/pages/addAd.php
@@ -74,6 +74,105 @@ if(!isset($_SESSION["userEmail"])){
 
                     if (mysqli_query($con, $sql)) {
                         echo "<div class='msg'>Post Added Successfully</div>";
+
+                        $mail = 'nilupul2001chalana@gmail.com';
+     
+
+                        $to         = $email;
+                        $mail_subject = 'Goviya.lk';
+                        $email_body   = "
+                        
+                    <!DOCTYPE html>
+                    <html lang='en'>
+                    <head>
+                        <meta charset='UTF-8'>
+                        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+                        <title>Document</title>
+                        <style>
+                    body
+                    {
+                        text-align: center;
+                        font-family: 'Gill Sans', 'Gill Sans MT', 'Myriad Pro', 'DejaVu Sans Condensed', Helvetica, Arial, 'sans-serif';
+                        
+                    }
+                    *{
+                        padding: 0px;
+                        margin: 0px;
+                        box-sizing: border-box;
+                    }
+                    .content
+                    {
+                        justify-content: center;
+                        flex: wrap;
+                        display:flex ;
+                        width: 100%;
+                        background-color: rgb(0, 0, 0);
+                        justify-content: center;
+                    padding:50px 20px;
+                    }
+                    .out
+                    {
+                        justify-content: center;
+                        flex: wrap;
+                        display:block ;
+                    }
+                    .banner
+                    {
+                        font-size: 30px;
+                        height: 50px;
+                        width: 100%;
+                        background-color: #ffb800;
+                        color: white;
+                        padding: 20px;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                    }
+                    .in
+                    {
+                        margin-top: 10px;
+                        color: rgb(255, 255, 255);
+                        font-size: 25px;
+                        width: 100%;
+                        padding: 20px;
+                    }
+
+                        </style>
+                    </head>
+                    <body>
+                        <div class='content'>
+                            <div class='out'>
+                                    <div class='banner'>
+                                        Goviya.lk
+                                    </div>
+                                    <div class='in'>
+                                        Your Ad was successfully posted, stay in touch
+                                    </div>
+                                    <div class='in' style='margin-top: 0;font-size:19px'>
+                                        cgraphyweb@gmail.com
+                                    </div>
+                            </div>
+
+                        </div>
+                    </body>
+                    </html>
+                        
+                        
+                        ";
+     
+
+      $header       = "From: {$mail}\r\nContent-Type: text/html;";
+
+      $send_mail_result = mail($to, $mail_subject, $email_body, $header);
+
+      if ($send_mail_result) {
+       
+      } else {
+        echo "Message Not Sent.";
+      }
+
+
                     } else {
                         echo "Error";
                     }
